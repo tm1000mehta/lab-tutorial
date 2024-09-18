322bbb69f95e12b4bc92fb696dcae1413da41978
G Steward <g.e.steward14@gmail.com>
2024-09-18 14:34:27 -0500

    Revert "Move reference line"
    This reverts commit c2d7af7cbb14f6cc5ba23334d59d57d46e2312e1.

--- sine_plot.py
+++ sine_plot.py
@@ -5,7 +5,8 @@ x = np.linspace(0,2*np.pi, 1000)
 y = np.sin(x)
 
 fig, ax = plt.subplots()
-ax.plot(x, np.zeros(len(x)), linestyle="--", color='0.8')
 ax.plot(x, y)
+
+ax.plot(x, np.zeros(len(x)), linestyle="--", color='0.8')
 
 plt.show()
