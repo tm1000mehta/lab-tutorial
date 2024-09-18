5fe50ce6509449e3b66a24ab4b80ccfc25e41371
G Steward <g.e.steward14@gmail.com>
2024-09-18 14:36:27 -0500

    Comments for sine_plot.py
    Added comments to clarify sine_plot.py

--- sine_plot.py
+++ sine_plot.py
@@ -1,12 +1,18 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
+# Create data for sine wave
 x = np.linspace(0,2*np.pi, 1000)
 y = np.sin(x)
 
+#Draw initial figure and axis
 fig, ax = plt.subplots()
+
+#Plot sine wave
 ax.plot(x, y)
 
+#Plot reference line
 ax.plot(x, np.zeros(len(x)), linestyle="--", color='0.8')
 
+#Show plot
 plt.show()
