c565704170f0407e983ff41d98ad083285ea240b
G Steward <g.e.steward14@gmail.com>
2024-09-23 12:42:35 -0500

    Formatting to work w/ file structure

--- 03_Scripts/04_FigureGeneration/sine_plot.py
+++ 03_Scripts/04_FigureGeneration/sine_plot.py
@@ -1,5 +1,12 @@
 from matplotlib import pyplot as plt
 import numpy as np
+import os
+
+## Path handling to find the parent directory based on location of this file
+scriptpath = os.path.realpath(__file__)
+parentDirInd = scriptpath.find("03_Scripts")
+parentdir = scriptpath[0:parentDirInd]
+os.chdir(parentdir)
 
 # Create data for sine wave
 x = np.linspace(0,2*np.pi, 1000)
@@ -15,4 +22,5 @@ ax.plot(x, y)
 ax.plot(x, np.zeros(len(x)), linestyle="--", color='0.8')
 
 #Show plot
-plt.show()
+plt.savefig(os.path.realpath("04_Outputs/01_Figures/sineplot.png"))
+plt.show(fig)
